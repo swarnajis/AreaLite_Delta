8581f134a2f576e567d64d6b7a1de12d154f9a51
swarnajis <swarnajis@gmail.com>
2025-06-28 14:37:04 -0400

    Update app.py

--- app.py
+++ app.py
@@ -54,4 +54,4 @@ def download_file(filename):
     return send_from_directory(app.config['OUTPUT_FOLDER'], filename, as_attachment=True)
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=40000)
+#    app.run(host="0.0.0.0", port=40000)
