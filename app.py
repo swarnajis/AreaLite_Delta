e80f1fa87ed93eb1232c6f4f65e1b6b390c772b4
swarnajis <swarnajis@gmail.com>
2025-06-28 13:59:07 -0400

    Add files via upload

--- app.py
+++ app.py
@@ -57,4 +57,4 @@ def download_file(filename):
     return send_from_directory(app.config['OUTPUT_FOLDER'], filename, as_attachment=True)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=10000)
